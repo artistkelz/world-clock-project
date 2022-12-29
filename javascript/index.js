218dc4459d42f0463188234ee581dc87de92d72b
artistkelz <93364676+artistkelz@users.noreply.github.com>
2022-12-28 19:23:26 -0700

    Update Timezones * update select timezones to include correct formatting * handle select city base case

--- javascript/index.js
+++ javascript/index.js
@@ -60,17 +60,24 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  let citiesElement = document.querySelector("#cities");
+
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
+
+  if (cityTimeZone === "reset") {
+    citiesElement.innerHTML = `<div/>`;
+    return;
+  }
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
     <div class="city">
     <div>
         <h2>${cityName}</h2>
-        <div class="date>${cityTime.format(
+        <div class="date">${cityTime.format(
           "h:mm:ss:"
         )} <small>${cityTime.format("A")}</small></div>
     </div>
