1d4870fa27cdcd3e5042857cce24193f10dbe456
artistkelz <93364676+artistkelz@users.noreply.github.com>
2022-12-29 22:41:42 -0700

    Added multiple locations

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,6 @@
 function updateTime() {
   // London
   let londonElement = document.querySelector("#london");
-
   if (londonElement) {
     let londonDateElement = londonElement.querySelector(".date");
     let londonTimeElement = londonElement.querySelector(".time");
@@ -60,7 +59,6 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  let citiesElement = document.querySelector("#cities");
 
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
@@ -73,13 +71,16 @@ function updateCity(event) {
 
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
     <div class="city">
     <div>
         <h2>${cityName}</h2>
-        <div class="date">${cityTime.format(
-          "h:mm:ss:"
-        )} <small>${cityTime.format("A")}</small></div>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+</div> 
+<div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
     </div>
     `;
 }
